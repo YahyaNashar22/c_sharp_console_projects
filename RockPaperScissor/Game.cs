cd95c00435d788b16b965b691e23d709e7030c56
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-08-05 08:50:53 +0300

    rock paper scissor done!

--- RockPaperScissor/Game.cs
+++ RockPaperScissor/Game.cs
@@ -3,14 +3,53 @@ using System;
 class Game
 {
   Random random = new Random();
-  string playerOption;
+  string playerOption = "";
   int enemyOption;
   int playerScore = 0;
   int enemyScore = 0;
+
+
   public static void IntroMessage()
   {
     Console.WriteLine("Welcome to the Rock, Paper, Scissor game!\nWe're excited to see you play!");
     Console.WriteLine("[r] - rock\n[p] - paper\n[s] - scissor");
+  }
+
+  public string GetPlayerOption()
+  {
+    return playerOption;
+  }
+
+  public int GetEnemyOption()
+  {
+    return enemyOption;
+  }
+
+  public int GetPlayerScore()
+  {
+    return playerScore;
+  }
+
+  public int GetEnemyScore()
+  {
+    return enemyScore;
+  }
+
+  public void PrintScore()
+  {
+    if (playerScore == 3)
+    {
+      Console.WriteLine("Congrats! You won the game!");
+    }
+    else
+    if (enemyScore == 3)
+    {
+      Console.WriteLine("Enemy Won! Better luck next time...");
+    }
+    else
+    {
+      Console.WriteLine($"Player: {playerScore}\tEnemy: {enemyScore}");
+    }
   }
 
   public void CapturePlayerChoice()
@@ -21,6 +60,7 @@ class Game
 
   public void CaptureEnemyChoice()
   {
+    Console.Clear();
     enemyOption = random.Next(0, 3);
 
     switch (enemyOption)
@@ -37,7 +77,7 @@ class Game
     }
   }
 
-    public void Score(string playerChoice, int enemyChoice)
+  public void Score(string playerChoice, int enemyChoice)
   {
     switch (playerChoice)
     {
@@ -48,47 +88,45 @@ class Game
         }
         if (enemyChoice == 1)
         {
-          Console.WriteLine("Draw!");
-
+          Console.WriteLine("Enemy wins!");
+          enemyScore++;
         }
         if (enemyChoice == 2)
         {
-          Console.WriteLine("Draw!");
-
+          Console.WriteLine("You win!");
+          playerScore++;
         }
         break;
       case "p":
         if (enemyChoice == 0)
         {
-          Console.WriteLine("Draw!");
-
+          Console.WriteLine("You win!");
+          playerScore++;
         }
         if (enemyChoice == 1)
         {
           Console.WriteLine("Draw!");
-
         }
         if (enemyChoice == 2)
         {
-          Console.WriteLine("Draw!");
-
+          Console.WriteLine("Enemy wins!");
+          enemyScore++;
         }
         break;
       case "s":
         if (enemyChoice == 0)
         {
-          Console.WriteLine("Draw!");
-
+          Console.WriteLine("Enemy wins!");
+          enemyScore++;
         }
         if (enemyChoice == 1)
         {
-          Console.WriteLine("Draw!");
-
+          Console.WriteLine("You win!");
+          playerScore++;
         }
         if (enemyChoice == 2)
         {
           Console.WriteLine("Draw!");
-
         }
         break;
     }
--- RockPaperScissor/Program.cs
+++ RockPaperScissor/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
   static void Main(string[] args)
   {
-    
+    Game.IntroMessage();
+    Game game = new Game();
+
+    while (game.GetPlayerScore() < 3 && game.GetEnemyScore() < 3)
+    {
+      game.CapturePlayerChoice();
+      game.CaptureEnemyChoice();
+      game.Score(game.GetPlayerOption(), game.GetEnemyOption());
+      game.PrintScore();
+      Console.WriteLine();
+    }
   }
 }
