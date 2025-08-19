a0c760aeba82cfc33fd37346ba6f99872a0b07e8
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-08-19 12:23:40 +0300

    started project: Tic Tac Toe

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+  static string[] board = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+  static void printBoard()
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 3; j++)
+      {
+        Console.Write(board[i * 3 + j] + "|");
+      }
+      Console.WriteLine();
+      Console.WriteLine("------");
+    }
+  }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Welcome To The Console Tic Tac Toe!");
+    Console.WriteLine("####################################");
+    Console.WriteLine();
+
+    printBoard();
+
+  }
+}
