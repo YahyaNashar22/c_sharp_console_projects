70fb00b9df2a4dc438436848aaa5da65eb4b3a18
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-08-21 13:33:03 +0300

    understanding delegates

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+  delegate void MyDelegate1();
+  delegate int MyDelegate2(int x, int y);
+  delegate string MyDelegate3(int x, int y);
+
+  static void Method1()
+  {
+    Console.WriteLine("I am method 1!");
+  }
+
+  static int Method2(int x, int y)
+  {
+    Console.WriteLine("I am method 2!");
+    return x + y;
+  }
+
+  static string Method3(int x, int y)
+  {
+    Console.WriteLine("I am method 3!");
+    return $"Adding {x} and {y} will return {x + y}";
+  }
+  static void Method4()
+  {
+    Console.WriteLine("I am method 4!");
+  }
+
+
+  static void Main(string[] args)
+  {
+    MyDelegate1? del1 = Method1;
+    MyDelegate2 del2 = Method2;
+    MyDelegate3 del3 = Method3;
+
+    del1();
+    Console.WriteLine("subscribed method 4 to del1");
+    del1 += Method4;
+    del1();
+    Console.WriteLine("un-subscribed method 4 from del1");
+    del1 -= Method1;
+    del1?.Invoke();
+    Console.WriteLine("-+-+-+-+-+-+-+-+-+");
+    int resultOfDel2 = del2(3, 4);
+    Console.WriteLine(resultOfDel2);
+    Console.WriteLine("-+-+-+-+-+-+-+-+-+");
+    Console.WriteLine(del3(4, 5));
+    Console.WriteLine("-+-+-+-+-+-+-+-+-+");
+  }
+}
