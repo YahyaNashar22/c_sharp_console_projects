78dcda5daa71cb84d9157d562f71e6234fdd3bb1
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-08-27 14:15:41 +0300

    fixed encoding to support emojis

--- MemoryMatch/Program.cs
+++ MemoryMatch/Program.cs
@@ -2,6 +2,8 @@
 {
   static void Main()
   {
+    Console.OutputEncoding = System.Text.Encoding.UTF8;
+
     var board = new Board(3, 4); // change dimensions if you like (must multiply to an even number)
     board.Play();
     Console.WriteLine("Press ENTER to exit.");
