3ce787a6c19cbdd5e5cb6a837275f470ccf7dd83
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-09-03 12:49:25 +0300

    finished project: hang man

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    string word = "game";
+
+    int maxLives = 7;
+    int currentLives = maxLives;
+
+    bool win = false;
+
+    List<char> guessedLetters = new List<char>();
+
+    while (currentLives > 0 && !win)
+    {
+      foreach (char c in word)
+      {
+        if (guessedLetters.Contains(c))
+        {
+          Console.Write(c);
+        }
+        else
+        {
+          Console.Write("_");
+        }
+      }
+      Console.WriteLine("\nPlease guess a letter!");
+      Console.WriteLine($"{currentLives}/{maxLives}");
+
+      char guess = Convert.ToChar(Console.ReadLine()!);
+
+      if (word.Contains(guess) && !guessedLetters.Contains(guess))
+      {
+        Console.WriteLine("Correct!");
+      }
+      else
+      {
+        Console.WriteLine("Incorrect!");
+        currentLives--;
+      }
+      guessedLetters.Add(guess);
+
+      bool wordComplete = true;
+
+      foreach (char c in word)
+      {
+        if (!guessedLetters.Contains(c))
+        {
+          wordComplete = false;
+        }
+      }
+      win = wordComplete;
+    }
+
+    if (win)
+    {
+      Console.WriteLine("Congrats! you win!");
+    }
+    else
+    {
+      Console.WriteLine("You lose . . .");
+    }
+
+  }
+}
